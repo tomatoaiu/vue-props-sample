437b8d827dbf409bd20bb35d9084674407a81efa
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2019-04-30 13:21:49 +0900

    feat(props): create props sample

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <ChildComponent :porps-for-child="{
+      from: 'Parent',
+      to: 'Child'
+    }"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ChildComponent from './components/ChildComponent.vue';
+
+@Component({
+  components: {
+    ChildComponent,
+  },
+})
+export default class App extends Vue {
+  porpsForChild = {
+    from: 'Parent',
+    to: 'Child',
+  }
+}
+</script>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>{{ from }} {{ to }}</p>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+type PorpsForChild = {
+  from: string,
+  to: string
+}
+
+@Component
+export default class ChildComponent extends Vue {
+  @Prop({ type: Object, required: true }) private porpsForChild!: PorpsForChild;
+
+  get from() {
+    return this.porpsForChild.from;
+  }
+
+  get to() {
+    return this.porpsForChild.to;
+  }
+}
+</script>
